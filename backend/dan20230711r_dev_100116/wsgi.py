a3fd2b994d055d09abb4c6669617d9289d0a8c4f
Crowdbotics <engineering@crowdbotics.com>
2023-07-11 13:52:38 +0000

    Initial commit.

---
+++ backend/dan20230711r_dev_100116/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dan20230711r_dev_100116 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dan20230711r_dev_100116.settings')
+
+application = get_wsgi_application()
